2aae3c90591ea2797fa3df5c07f6762a711177c1
otumian <popecan1000@gmail.com>
2020-08-07 19:34:12 +0000

    renamed index.js to app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const onboardingController = require('./routes/onboardingroutes.js')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.json());
 app.use(express.static('./public'))
 
 onboardingController(app)
