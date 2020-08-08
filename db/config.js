309ef5cdcddd037ed84f3b508a0d4863aa3b4e84
otumian <popecan1000@gmail.com>
2020-08-08 15:05:11 +0000

    Update db/config.js by moving function that hash and compare hashed password into db/encrypt.js Update db/onboardingdb.js to return directly the client.query result

--- db/config.js
+++ db/config.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+
 const { Client } = require('pg')
 
 const client = new Client({
@@ -6,9 +7,13 @@ const client = new Client({
     host: process.env.PG_DBHOST,
     database: process.env.PG_DBNAME,
     password: process.env.PG_DBPASSWORD,
-    port: process.env.PG_DBPORT,
+    port: process.env.PG_DBPORT || 3000,
 })
 
 client.connect()
+    .then(result => {
+        console.log(`DB connected ${result}`)
+    })
+    .catch(err => console.log("Connection error " + err.stack))
 
-module.exports = client
+module.exports = { client }
